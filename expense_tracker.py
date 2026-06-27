e7492f5c5af87448d0ae61278f241ec2df5d5a38
tshepisoraphunga-beep <tshepisoraphunga@gmail.com>
2026-06-27 18:18:55 +0200

    Add files via upload

---
+++ expense_tracker.py
@@ -0,0 +1,12 @@
+total = 0
+
+expense1 = float(input("Enter expense 1: R"))
+total = total + expense1
+
+expense2 = float(input("Enter expense 2: R"))
+total = total + expense2
+
+expense3 = float(input("Enter expense 3: R"))
+total = total + expense3
+
+print("Total spent: R", total)
